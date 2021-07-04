b3f87201f1e406d67daedbbb150c804a4f885fea
phuongtt <tuanphuongtran.14@gmail.com>
2021-07-04 08:50:36 +0700

    Add new backend folder

---
+++ packages/backend/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    email: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'tuanphuongtran.14@gmail.com',
+        defaultReplyTo: 'tuanphuongtran.14@gmail.com'
+      },
+    },
+  });
---
+++ packages/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '843be088-efe1-4228-b6ba-464d22ebbf99'
+};
