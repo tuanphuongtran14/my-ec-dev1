a21b0195ed59e375361e4c425f6a7c0c3d796b99
unknown <quachtuananh2016@gmail.com>
2021-07-09 16:47:53 +0700

    get data from graphql for cart

--- packages/ec-web/components/Cart/Cart.js
+++ packages/ec-web/components/Cart/Cart.js
@@ -1,21 +1,120 @@
 import React from "react";
+import {
+  ApolloClient, HttpLink,ApolloProvider, ApolloLink, InMemoryCache, concat,useQuery,
+  gql
+} from "@apollo/client";
+
+const httpLink = new HttpLink({ uri: 'http://localhost:1337/graphql' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZTgxMmYyMzkzOGU5MzRiNGE5ZjBmZiIsImlhdCI6MTYyNTgyMjA0MiwiZXhwIjoxNjI4NDE0MDQyfQ.moLVkY18c8mAsybvXXLJju7gNIXP30mvbOuwLvAhWFA"||null,
+    }
+  }));
+
+  return forward(operation);
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
+
+
+const EXCHANGE_RATES = gql`
+  query {
+    getCart {
+      items {
+        id
+        product {
+          name,
+          price,
+          thumbnail {
+            url
+          }
+        },
+        color,
+        qty,
+        price
+      }
+      user {
+        username
+      }
+    }
+  }
+`;
+
+function ProductItem() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.getCart.items.map((item) => (
+    <div className="items" key={item.id}>
+      <div className="check-item">
+        <div className="check-item-left">
+          <input type="checkbox" className="checkbox-wrap__input" />
+          <img
+            src="https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-didongviet_1.jpg"
+            alt="iphone"
+            className="check-item-left__img"
+          />
+          <span>{item.product.name}</span>
+        </div>
+        <div className="check-item-middle">
+          <div className="btn-group middle-btn">
+            <button type="button" className="btn btn-outline-secondary">
+              -
+            </button>
+            <button type="button" className="btn btn-outline-dark">
+              1
+            </button>
+            <button type="button" className="btn btn-outline-secondary">
+              +
+            </button>
+          </div>
+          <div className="middle-color">
+            <label htmlFor="select-color">Màu:</label>
+            <select name="color" id="select-color">
+              <option value="den">Đen</option>
+              <option value="hong">Hồng</option>
+              <option value="trang">Trắng</option>
+              <option value="xanh">Xanh</option>
+            </select>
+          </div>
+        </div>
+        <div className="check-item-right">
+          <span>{item.price}đ</span>
+          <div className="item-right-icon">
+            <i className="far fa-heart"></i>
+            <i className="far fa-trash-alt"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+}
 
 const Cart = () => {
   return (
-    <>
-      <div className="all">       
-          <nav aria-label="breadcrumb">
-            <ol className="breadcrumb mb-0" >
-              <div className="grid wide d-flex">
-                <li className="breadcrumb-item">
-                    <h6>Home</h6>
-                </li>
-                <li className="breadcrumb-item">
-                    <h6>Cart</h6>
-                </li>
-              </div>
-            </ol>
-          </nav>
+    <ApolloProvider client={client}>
+      <div className="all">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb mb-0">
+            <div className="grid wide d-flex">
+              <li className="breadcrumb-item">
+                <h6>Home</h6>
+              </li>
+              <li className="breadcrumb-item">
+                <h6>Cart</h6>
+              </li>
+            </div>
+          </ol>
+        </nav>
         <div className="cart">
           <div className="grid wide">
             <div className="row">
@@ -36,150 +135,7 @@ const Cart = () => {
                     <span>Xóa</span>
                   </div>
                 </div>
-                <div className="items">
-                  <div className="check-item">
-                    <div className="check-item-left">
-                      <input type="checkbox" className="checkbox-wrap__input" />
-                      <img
-                        src="https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-didongviet_1.jpg"
-                        alt="iphone"
-                        className="check-item-left__img"
-                      />
-                      <span>iPhone 12 128GB Chính hãng (VN/A)</span>
-                    </div>
-                    <div className="check-item-middle">
-                      <div className="btn-group middle-btn">
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          -
-                        </button>
-                        <button type="button" className="btn btn-outline-dark">
-                          1
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          +
-                        </button>
-                      </div>
-                      <div className="middle-color">
-                        <label htmlFor="select-color">Màu:</label>
-                        <select name="color" id="select-color">
-                          <option value="den">Đen</option>
-                          <option value="hong">Hồng</option>
-                          <option value="trang">Trắng</option>
-                          <option value="xanh">Xanh</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="check-item-right">
-                      <span>25.000.000đ</span>
-                      <div className="item-right-icon">
-                        <i className="far fa-heart"></i>
-                        <i className="far fa-trash-alt"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="items">
-                  <div className="check-item">
-                    <div className="check-item-left">
-                      <input type="checkbox" className="checkbox-wrap__input" />
-                      <img
-                        src="https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-didongviet_1.jpg"
-                        alt="iphone"
-                        className="check-item-left__img"
-                      />
-                      <span>iPhone 12 128GB Chính hãng (VN/A)</span>
-                    </div>
-                    <div className="check-item-middle">
-                      <div className="btn-group middle-btn">
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          -
-                        </button>
-                        <button type="button" className="btn btn-outline-dark">
-                          1
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          +
-                        </button>
-                      </div>
-                      <div className="middle-color">
-                        <label htmlFor="select-color">Màu:</label>
-                        <select name="color" id="select-color">
-                          <option value="den">Đen</option>
-                          <option value="hong">Hồng</option>
-                          <option value="trang">Trắng</option>
-                          <option value="xanh">Xanh</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="check-item-right">
-                      <span>25.000.000đ</span>
-                      <div className="item-right-icon">
-                        <i className="far fa-heart"></i>
-                        <i className="far fa-trash-alt"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="items">
-                  <div className="check-item">
-                    <div className="check-item-left">
-                      <input type="checkbox" className="checkbox-wrap__input" />
-                      <img
-                        src="https://didongviet.vn/pub/media/catalog/product//i/p/iphone-12-didongviet_1.jpg"
-                        alt="iphone"
-                        className="check-item-left__img"
-                      />
-                      <span>iPhone 12 128GB Chính hãng (VN/A)</span>
-                    </div>
-                    <div className="check-item-middle">
-                      <div className="btn-group middle-btn">
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          -
-                        </button>
-                        <button type="button" className="btn btn-outline-dark">
-                          1
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-secondary"
-                        >
-                          +
-                        </button>
-                      </div>
-                      <div className="middle-color">
-                        <label htmlFor="select-color">Màu:</label>
-                        <select name="color" id="select-color">
-                          <option value="den">Đen</option>
-                          <option value="hong">Hồng</option>
-                          <option value="trang">Trắng</option>
-                          <option value="xanh">Xanh</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="check-item-right">
-                      <span>25.000.000đ</span>
-                      <div className="item-right-icon">
-                        <i className="far fa-heart"></i>
-                        <i className="far fa-trash-alt"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ProductItem/>
               </div>
               <div className="col l-4 md-4 c-12 cart-checkout">
                 <h4>Thông tin đơn hàng</h4>
@@ -218,7 +174,7 @@ const Cart = () => {
           </div>
         </div>
       </div>
-    </>
+    </ApolloProvider>
   );
 };
 
