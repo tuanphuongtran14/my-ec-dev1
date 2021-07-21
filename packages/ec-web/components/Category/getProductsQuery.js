3bddacfe2e4a4c7405d95f47590ac6047649d26d
unknown <quachtuananh2016@gmail.com>
2021-07-21 13:38:45 +0700

    get api index

--- packages/ec-web/components/Category/getProductsQuery.js
+++ packages/ec-web/components/Category/getProductsQuery.js
@@ -18,7 +18,8 @@ export default function query() {
                 brand{
                     name,
                     slug
-                }
+                },
+                updatedAt
             }
         }
   `
--- packages/ec-web/pages/index.js
+++ packages/ec-web/pages/index.js
@@ -22,14 +22,113 @@ export async function getServerSideProps() {
   };
 }
 
+
 export default function Home({ products }) {
 
-  const ProductItem = products.map((product) => {
+  const productHotSale = products.filter(product => product.sales_percentage > 10)
+  
+  productHotSale.sort(function (a, b) {
+    return b.sales_percentage - a.sales_percentage;
+  });
+
+  const productNew = products.filter(product =>{
+    const newDate = new Date(product.updatedAt);
+    const date = new Date();
+    return (
+      (newDate.getMonth() - date.getMonth()) === 0
+    )
+  })
+
+  const HotSale = productHotSale.map((product) => {
     const regularPrice = product.regular_price.toLocaleString("DE-de");
     const finalPrice = product.final_price.toLocaleString("DE-de");
 
     return (
-      <Link href="/product/[slug]" as={`/product/${product.slug}`}>
+      <Link href="/product/[slug]" as={`/product/${product.slug}`} key={product.id}>
+        <div className="product">
+          <img src={process.env.NEXT_PUBLIC_API_URL + product.thumbnail.url} alt="" className="product__img mb-4" style={{ maxHeight: "204px", maxWidth: "204px"}}/>
+          <span className="product__title">
+            <Link href="/product/[slug]" as={`/product/${product.slug}`} className="text-dark">
+              {product.name}
+            </Link>
+          </span>
+          <div className="product__price">
+            {product.sales_percentage === 0 ?
+              null :
+              <span className="sales-price">{finalPrice}₫</span>
+            }
+            <span className="regular-price">{regularPrice}₫</span>
+          </div>
+          <div className="product__rating">
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon  fa-star-half" aria-hidden="true"></i>
+            <span>(472 đánh giá)</span>
+          </div>
+          {
+            product.sales_percentage != 0 ?
+              <div className="product__box-sticker">
+                <p className="sticker-percent">{product.sales_percentage}%</p>
+              </div>
+              : null
+          }
+
+        </div>
+      </Link>
+    )
+  }
+  )
+
+  const productsNew = productNew.map((product) => {
+    const regularPrice = product.regular_price.toLocaleString("DE-de");
+    const finalPrice = product.final_price.toLocaleString("DE-de");
+
+    return (
+      <Link href="/product/[slug]" as={`/product/${product.slug}`} key={product.id}>
+        <div className="product">
+          <img src={process.env.NEXT_PUBLIC_API_URL + product.thumbnail.url} alt="" className="product__img mb-4" style={{ maxHeight: "204px", maxWidth: "204px"}}/>
+          <span className="product__title">
+            <Link href="/product/[slug]" as={`/product/${product.slug}`} className="text-dark">
+              {product.name}
+            </Link>
+          </span>
+          <div className="product__price">
+            {product.sales_percentage === 0 ?
+              null :
+              <span className="sales-price">{finalPrice}₫</span>
+            }
+            <span className="regular-price">{regularPrice}₫</span>
+          </div>
+          <div className="product__rating">
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon fa-star" aria-hidden="true"></i>
+            <i className="fa product__rating-icon  fa-star-half" aria-hidden="true"></i>
+            <span>(472 đánh giá)</span>
+          </div>
+          {
+            product.sales_percentage != 0 ?
+              <div className="product__box-sticker">
+                <p className="sticker-percent">{product.sales_percentage}%</p>
+              </div>
+              : null
+          }
+
+        </div>
+      </Link>
+    )
+  }
+  )
+
+  const bestSeller  = products.map((product) => {
+    const regularPrice = product.regular_price.toLocaleString("DE-de");
+    const finalPrice = product.final_price.toLocaleString("DE-de");
+
+    return (
+      <Link href="/product/[slug]" as={`/product/${product.slug}`} key={product.id}>
         <div className="product">
           <img src={process.env.NEXT_PUBLIC_API_URL + product.thumbnail.url} alt="" className="product__img mb-4" style={{ maxHeight: "204px", maxWidth: "204px"}}/>
           <span className="product__title">
@@ -95,7 +194,10 @@ export default function Home({ products }) {
               reloadOnUpdate // default false
               static // default false
           >
-           {ProductItem}
+           {typeCategory === 'Hot sales' ? HotSale :
+            typeCategory === 'Bán chạy' ? bestSeller :
+            typeCategory === 'Mới nhất' ? productsNew :''
+          }
           </Flickity>
         </div>
       </div>
