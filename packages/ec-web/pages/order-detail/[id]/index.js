3d48ec32c8f7eef587c8e45bf160448a16c725ce
SaDo21042000 <dovansa21042000@gmail.com>
2021-08-09 09:01:04 +0700

    task SaDo

--- packages/ec-web/pages/order-detail/[id]/index.js
+++ packages/ec-web/pages/order-detail/[id]/index.js
@@ -20,7 +20,10 @@ export const getServerSideProps = async ({ params }) => {
                                 url
                             }
                         }
+                        unitPrice
+                        totalPrice
                         qty
+                        color
                     }
                     coupon{
                         discountAmount
@@ -46,19 +49,24 @@ export default function OrderDetail({ order }) {
 
     const item = order.items.map(item => {
         return (
-            <tr>
-                <td>
-                    <div class="product-item">
-                            <img src={process.env.NEXT_PUBLIC_API_URL + item.product.thumbnail.url}  style={{width: "20vh"}}/>
-                        <span class="product-name">{item.product.name}</span>
-                    </div>
-                </td>
-                <td class="price">{item.product.regularPrice.toLocaleString("DE-de")} ₫</td>
-                <td>{order.status}</td>
-                <td class="quantity">{item.qty}</td>
-                <td class="discount-amount">{order.coupon === null ? 0 : order.coupon.discountAmount.toLocaleString("DE-de")} ₫</td>
-                <td class="raw-total">{item.product.finalPrice.toLocaleString("DE-de")} ₫</td>
-            </tr>
+             <div class="row order-detail__item">
+                <div class="col-sm-4 col-info col-info--product">
+                    <img class="order-detail__img" src={process.env.NEXT_PUBLIC_API_URL + item.product.thumbnail.url} />
+                    <span class="order-detail__info">{item.product.name}</span>
+                </div>
+                <div class="col-sm-2 col-info">
+                    <p class="order-detail__info">{item.unitPrice.toLocaleString("DE-de")} ₫</p>
+                </div>
+                <div class="col-sm-2 col-info">
+                    <p class="order-detail__info">{item.color}</p>
+                </div>
+                <div class="col-sm-2 col-info">
+                    <p class="order-detail__info">{item.qty}</p>
+                </div>
+                <div class="col-sm-2 col-info">
+                    <p class="order-detail__info">{item.totalPrice.toLocaleString("DE-de")} ₫</p>
+                </div>
+            </div>
         )
     })
 
@@ -66,7 +74,7 @@ export default function OrderDetail({ order }) {
         <div>
             <nav class="breadcrumb breadcrumb--custom my-1">
                 <div class="container px-0">
-                    <a class="breadcrumb-item" href="#">Trang chủ</a>
+                    <a class="breadcrumb-item" href="/">Trang chủ</a>
                     <span class="breadcrumb-item active">Quản lý đơn hàng</span>
                 </div>
             </nav>
@@ -78,57 +86,59 @@ export default function OrderDetail({ order }) {
                             <div>
                                 <h2 class="bill-detail__title">Chi tiết đơn hàng #{order.orderId}</h2>
                             </div>
-                            <div class="shadow" style={{ backgroundColor: "#fff", padding: '26px' }}>
-                                <span class="button-link">
-                                    <a href="">Đánh giá</a>
-                                </span>
-                                <span class="button-link">
-                                    <a href="">Mua lại</a>
-                                </span>
-                                <table style={{ width: '100%' }} class="bill-detail">
-
-                                    <thead>
-                                        <tr>
-                                            <th>Sản phẩm</th>
-                                            <th>Giá</th>
-                                            <th>Trạng thái</th>
-                                            <th>Số lượng</th>
-                                            <th>Giảm giá</th>
-                                            <th>Tạm tính</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {item}
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <td colspan="4"><span><b>Tạm tính:</b></span></td>
-                                            <td>{order.finalAmount.toLocaleString("DE-de")} ₫</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="4"><span><b>Phí vận chuyển:</b></span></td>
-                                            <td>0 ₫</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="4"><span><b>Tổng cộng:</b></span></td>
-                                            <td><span class="sum">{order.finalAmount.toLocaleString("DE-de")} ₫</span></td>
-                                        </tr>
-
-                                    </tfoot>
-                                </table>
+                            <div class="container order-detail">
+                                {/*<div class="row ">
+                                    <div class="link-button">
+                                        <div class="link-button__item">
+                                            <a href="">Đánh giá</a>
+                                        </div>
+                                        <div class="link-button__item">
+                                            <a href="">Mua lại</a>
+                                        </div>
+                                    </div>
+                                </div>*/}
+                                <div class="row order-detail__heading">
+                                    <div class="col-sm-4 order-detail__title">
+                                        <h5>Sản phẩm</h5>
+                                    </div>
+                                    <div class="col-sm-2 order-detail__title">
+                                        <h5>Giá</h5>
+                                    </div>
+                                    <div class="col-sm-2 order-detail__title">
+                                        <h5>Màu sắc</h5>
+                                    </div>
+                                    <div class="col-sm-2 order-detail__title">
+                                        <h5>Số lượng</h5>
+                                    </div>
+                                    <div class="col-sm-2 order-detail__title">
+                                        <h5>Tạm tính</h5>
+                                    </div>
+                                </div>
+                                 {item}
+                                <div class="row final-price">
+                                    <div class="col-sm-3 final-price__title">
+                                        <p><b>Tạm tính: </b></p>
+                                        <p><b>Phí vận chuyển: </b></p>
+                                        <p><b>Tổng cộng: </b></p>
+                                    </div>
+                                    <div class="col-sm-2 final-price__value">
+                                        <p>{order.finalAmount.toLocaleString("DE-de")} ₫</p>
+                                        <p>0 đ</p>
+                                        <p> {order.finalAmount.toLocaleString("DE-de")} ₫</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-
+    );
     return (
         <div id="root">
             <Head>
                 <title>Chi tiết đơn hàng</title>
-                <link rel="stylesheet" href="./css/customer-info.css" />
+                <link rel="stylesheet" href="../../css/customer-info.css" />
             </Head>
             <Header />
             <OrderDetail />
